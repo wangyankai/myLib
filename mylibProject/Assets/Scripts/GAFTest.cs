1a4a4ace066f249150ca281ca2db1ec33d64f6bb
yankaiwang <yankai.wang@ihandysoft.com>
2015-05-21 15:23:16 +0800

    新资源

---
+++ mylibProject/Assets/Scripts/GAFTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using GAF.Core;
+
+public class GAFTest : MonoBehaviour {
+	public GAFMovieClip robot;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void testClick(){
+		robot.setSequence ("stand_left",true);
+	}
+}
--- mylibProject/Assets/Scripts/Sky/SkyAnimation/SkySetParticleSortingLayer.cs
+++ mylibProject/Assets/Scripts/Sky/SkyAnimation/SkySetParticleSortingLayer.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[ExecuteInEditMode]
 public class SkySetParticleSortingLayer : MonoBehaviour {
 
 	public string sortingLayerName="Default";
@@ -8,12 +8,25 @@ public class SkySetParticleSortingLayer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		particleSystem.renderer.sortingLayerName = sortingLayerName;
-		particleSystem.renderer.sortingOrder = sortingOrder;
+		Onchanged ();
 	}
-	
+	#if UNITY_EDITOR
 	// Update is called once per frame
 	void Update () {
-	
+//		if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode) {
+//			this.enabled = false;
+//		} else {
+			Onchanged();
+//		}
+	}
+	#else
+
+	#endif
+
+	private void Onchanged(){
+		if (particleSystem.renderer.sortingLayerName != sortingLayerName ||particleSystem.renderer.sortingOrder != sortingOrder) {
+			particleSystem.renderer.sortingLayerName = sortingLayerName;
+			particleSystem.renderer.sortingOrder = sortingOrder;
+		}
 	}
 }
